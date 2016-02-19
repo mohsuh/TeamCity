799e470c68e6b45c467c5982320b1b256f38b8c5
MOHAMMED SUHAIL <md.suhail@live.com>
2016-02-19 15:11:20 +0530

    First commit

---
+++ TCDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TCDemo.Startup))]
+namespace TCDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
